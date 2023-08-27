70b849cdeea40421b69a8fafe5a58b98cf8bfee5
hany elnasharty <hany.m.elnasharty@gmail.com>
2023-08-26 21:18:00 -0400

    updates

---
+++ src/components/Swift-modal.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player'
+
+const SwiftModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button className="open-modal-button" onClick={openModal}>Preview</button>
+
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span onClick={closeModal} className="close-button">
+              &times;
+            </span>
+                <ReactPlayer
+                    url={'/videos/SWIFTSHOP.mov'} 
+                    controls={true} 
+                    height='700px'
+                    width='480px'
+                    className="video"
+                />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SwiftModal;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,27 @@
 import React from "react"
+import ListingzModal from "../components/Listingz-modal"
+import EmpireModal from "../components/Empire-modal"
+import SwiftModal from "../components/Swift-modal"
+import XpoegModal from "../components/Xpoeg-modal"
 
 const Projects = () => {
     return (
         <>
             <div className="projects-container">
                 <h1 id="projects">Projects</h1>
-                <div className="projectss">
+                <div className="projects">
                     <h2><a href="https://github.com/hanyelnasharty/Listingz" target="_blank" rel="noreferrer">Listingz</a> | <span id="info">A full CRUD Real Estate listing app</span></h2>
                     <span>Node.js, Express.js, Mongoose, EJS</span>
+                    <ListingzModal />
                     <h2><a href="https://github.com/hanyelnasharty/empire-motors" target="_blank" rel="noreferrer">Empire Motors</a> | <span id="info">A full CRUD Car listing app</span></h2>
                     <span>Mongoose, Express.js, React.js, Node.js</span>
+                    <EmpireModal />
                     <h2><a href="https://github.com/smmyl/ecommerce-frontend" target="_blank" rel="noreferrer">SwiftShop</a> | <span id="info">A full CRUD E-commerce mockup app</span></h2>
                     <span>Python, Django, React.js, PSQL, Bootstrap</span>
+                    <SwiftModal />
                     <h2><a href="https://github.com/hanyelnasharty/xpoeg-frontend" target="_blank" rel="noreferrer">XPOEG</a> | <span id="info">A full CRUD Travel Guide app</span></h2>
                     <span>Python, Django, React.js, PSQL</span>
+                    <XpoegModal />
                 </div>
             </div>
             
